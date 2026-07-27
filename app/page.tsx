c294869173f15ee104c1215e0b6b37d88b3eac8a
mohammadhasansojib <sajibhasan800080@gmail.com>
2026-07-27 21:29:55 +0600

    build root layout and page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,29 @@
-import {Card} from "@/components/ui/card"
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "@/components/ui/card"
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
       
-      <Card className="w-80 p-5 m-5 bg-gray-50 shadow-xl">
-        <Image src={`/globe.svg`} alt="next-image" width="100" height="100" className="m-1" />
-        <h1 className="text-3xl font-bold">Title Here</h1>
-        <p className="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, deleniti!</p>
-        <Button variant={"default"} size="lg" className="cursor-pointer">Click Here</Button>
+      <Card className="max-w-sm m-5 p-5">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Project Overview</CardTitle>
+          <CardDescription>
+            Track progress and recent activity for your Next.js app.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam sapiente provident, aspernatur totam in necessitatibus modi architecto nam. Id, vel?
+        </CardContent>
+        <Button className={`cursor-pointer`}>Click Here</Button>
       </Card>
+
     </div>
   );
 }
